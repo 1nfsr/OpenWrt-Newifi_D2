1a080e80af1245ded14ee5954715d8472a89a069
Infsr <hannes.yuen@gmail.com>
2022-03-18 00:39:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,4 @@ sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_genera
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/other/luci-app-openclash
 svn co https://github.com/kongfl888/luci-app-adguardhome/trunk/ package/other/luci-app-adguardhome
-svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/other/luci-app-mosdns
-svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns package/other/mosdns
+svn co https://github.com/QiuSimons/openwrt-mos/trunk/ package/other/openwrt-mos
