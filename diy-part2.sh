fde9d05a69c9bc4969a85c73b7c034353eb7303f
Infsr <hannes.yuen@gmail.com>
2022-03-26 04:50:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,3 @@ sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_genera
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/other/luci-app-openclash
 svn co https://github.com/kongfl888/luci-app-adguardhome/trunk/ package/other/luci-app-adguardhome
-svn co https://github.com/QiuSimons/openwrt-mos/trunk/ package/other/openwrt-mos
