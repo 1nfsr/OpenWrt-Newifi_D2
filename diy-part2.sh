ff81332461cb6651bfaab000bd70a600d9bcb2c1
Infsr <i@infsr.me>
2022-04-22 01:38:42 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,10 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/o
 svn co https://github.com/kongfl888/luci-app-adguardhome/trunk/ package/other/luci-app-adguardhome
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/ package/other/openwrt-mos
 
+mv ${GITHUB_WORKSPACE}/pkg/* package/other/
+
+#sed -i '44 a$(curdir)/upx/compile := $(curdir)/ucl/compile' openwrt/tools/Makefile
+
 rm -rf tools/Makefile
 mv ${GITHUB_WORKSPACE}/tools/* tools/
 rm -rf feeds/packages/net/nginx-util/files/nginx.config
