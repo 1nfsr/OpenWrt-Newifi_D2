ccc6b917376f9ecc50a1d47d8318e9e0ecaf4947
Infsr <i@infsr.me>
2022-04-22 15:18:53 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,6 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/other/luci-app-openclash
 svn co https://github.com/kongfl888/luci-app-adguardhome/trunk/ package/other/luci-app-adguardhome
